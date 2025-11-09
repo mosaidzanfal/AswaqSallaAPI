4db2f32cfb6d3405010d5143c23786132b4502c9
Magdi Elsharkawi <magdyajyalAsus2025@outlook.com>
2025-11-09 13:37:00 +0300

    1

--- AswaqSallaAPI/Form1.Designer.cs
+++ AswaqSallaAPI/Form1.Designer.cs
@@ -16,6 +16,7 @@
         {
             this.btnImport = new System.Windows.Forms.Button();
             this.txtLog = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnImport
@@ -40,9 +41,19 @@
             this.txtLog.Size = new System.Drawing.Size(660, 300);
             this.txtLog.TabIndex = 1;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(258, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(180, 40);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Import from Salla-Modi";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.ClientSize = new System.Drawing.Size(684, 381);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.txtLog);
             this.Controls.Add(this.btnImport);
             this.Name = "Form1";
@@ -51,5 +62,7 @@
             this.PerformLayout();
 
         }
+
+        private System.Windows.Forms.Button button1;
     }
 }
